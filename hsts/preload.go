e51b0f5b5191f0cb954d7e252c133af642b1450b
coffeemakr <git@unstable.ch>
2021-03-25 14:29:45 +0100

    chromium /

--- hsts/preload.go
+++ hsts/preload.go
@@ -1,80 +1,26 @@
 package hsts
 
 import (
-	"bytes"
-	"context"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
+	"github.com/chromium/hstspreload/chromium/preloadlist"
 	https "github.com/coffeemakr/httpsify"
-	"log"
-	"net/http"
-	"sort"
 	"strings"
-	"time"
-
-	filter "github.com/tmthrgd/go-filter"
 )
 
-const jsonURL = "https://chromium.googlesource.com/chromium/src/net/+/master/http/transport_security_state_static.json?format=TEXT"
-
 func LoadHstsPreload() (*https.RuleCollection, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, jsonURL, nil)
+	preloadList, err := preloadlist.NewFromLatest()
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("Loading preload JSON")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned unexpected %s status code", resp.Status)
-	}
-
-	br := filter.NewReader(
-		base64.NewDecoder(base64.StdEncoding, resp.Body),
-		func(line []byte) bool {
-			line = bytes.TrimSpace(line)
-			return !(len(line) >= 2 && string(line[:2]) == "//")
-		})
-
-	var rules struct {
-		Entries []struct {
-			Name string
-
-			IncludeSubdomains bool `json:"include_subdomains"`
-
-			Mode string
-			//  "force-https" iff covered names should require HTTPS.
-		}
-	}
-	if err := json.NewDecoder(br).Decode(&rules); err != nil {
-		return nil, err
-	}
-
-	log.Println("Generating rules")
-	sort.Slice(rules.Entries, func(i, j int) bool {
-		ei, ej := rules.Entries[i], rules.Entries[j]
-		if ei.IncludeSubdomains != ej.IncludeSubdomains {
-			return ei.IncludeSubdomains
-		}
-		return ei.Name < ej.Name
-	})
+	names := make([]string, 0, len(preloadList.Entries))
+	includeSubdomainsNames := make([]string, 0, len(preloadList.Entries))
 
 	var maxDots, dots int
-	names := make([]string, 0, len(rules.Entries))
-	includeSubdomainsNames := make([]string, 0, len(rules.Entries))
-	for _, entry := range rules.Entries {
+	for _, entry := range preloadList.Entries {
 		if entry.Mode != "force-https" {
 			continue
 		}
-		if entry.IncludeSubdomains {
+		if entry.IncludeSubDomains {
 			includeSubdomainsNames = append(includeSubdomainsNames, entry.Name)
 		} else {
 			names = append(names, entry.Name)
--- httpsify.go
+++ httpsify.go
@@ -2,7 +2,7 @@ package httpsify
 
 var preloadedRules *RuleCollection
 
-// Rewrite rewrites urls using the HSTS preload list and https-everywhere rules.
+// Rewrite rewrites urls using the HSTS Preload list and https-everywhere rules.
 func Rewrite(url string) (string, error) {
 	url, _ = preloadedRules.Rewrite(url)
 	return url, nil
