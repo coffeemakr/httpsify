086ba7a87a2b6604662ef387e1224c26ebe9fa00
Cyril Müller <cmueller@woz.ch>
2021-10-19 10:35:00 +0200

    Update rules generation for https-everywhere submodule path

--- generate_rules.go
+++ generate_rules.go
@@ -2,4 +2,4 @@
 
 package httpsify
 
-//go:generate go run -tags generate script/generate-files.go --code-package=httpsify --https-everywhere-rules=../https-everywhere/rules --code-out=rules.go
+//go:generate go run -tags generate script/generate-files.go --code-package=httpsify --https-everywhere-rules=https-everywhere/rules --code-out=rules.go
